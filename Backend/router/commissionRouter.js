2e9d361f956c3e284a31e52ebb75c46793b3bd22
Mahima Patel <mahimaptel@gmail.com>
2025-10-28 23:48:31 +0530

    Make backend folder for deployment

--- Backend/router/commissionRouter.js
+++ Backend/router/commissionRouter.js
@@ -5,5 +5,4 @@ import { isAuthenticated,isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/proof", isAuthenticated, isAuthorized("Auctioneer"),proofOfCommission);
-
 export default router;
