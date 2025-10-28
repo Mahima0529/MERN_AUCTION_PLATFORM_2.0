a16ded915d170653eefca7be7e73c220a3886792
Mahima Patel <mahimaptel@gmail.com>
2025-10-28 23:48:15 +0530

    Make backend folder for deployment

--- Backend/router/bidRoutes.js
+++ Backend/router/bidRoutes.js
@@ -3,7 +3,6 @@ import { placeBid } from "../controllers/bidController.js";
 import { isAuthenticated , isAuthorized } from "../middlewares/auth.js";
 import { checkAuctionEndTime } from "../middlewares/checkAuctionEndTime.js";
 
-
 const router = express.Router();
 
 router.post("/place/:id", isAuthenticated, isAuthorized("Bidder"),checkAuctionEndTime, placeBid);
