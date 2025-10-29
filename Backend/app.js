3c8681ff3cec31cc311b669953af88b7565039aa
Mahima0529 <mahimaptel@gmail.com>
2025-10-29 11:52:10 +0530

    Add components for  Upcommig Auctions

--- Backend/app.js
+++ Backend/app.js
@@ -112,9 +112,8 @@ app.use("/api/v1/commission", commissionRouter);
 app.use("/api/v1/superadmin", superAdminRouter);
 
 
-// ✅ Cron jobs
-// =======
-// >>>>>>> a07a9c9ac4e446ca3f9342725fae65b7589debac
+
+
 endedAuctionCron();
 verifyCommissionCron();
 
--- frontend/src/pages/home-sub-components/UpcomingAuctions.jsx
+++ frontend/src/pages/home-sub-components/UpcomingAuctions.jsx
@@ -16,27 +16,33 @@ const UpcomingAuctions = () => {
 
   return (
     <>
-      <section className="my-8">
-        <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl ml-0">
-          Auctions For Today
-        </h3>
-        <div className="flex flex-wrap gap-6">
-          <div  className="bg-[#161613] w-full p-2 gap-10 rounded-md flex flex-col justify-between lg:flex-1 lg:h-auto lg:p-6 2xl:flex-none 2xl:basis-64 2xl:flex-grow 2xl:px-2  2xl:py-6">
-            <span className="rounded-full bg-[#fdba88] text-white w-fit p-3">
-              <RiAuctionFill />
-            </span>
-            <div>
-              <h3 className="text-[#fdba88] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
-                Auctions For
-              </h3>
-              <div>
-                <h3 className="text-[#fff] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl">
-                  Today
-                </h3>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 w-full lg:flex-1 2xl:flex-none 2xl:basis-64 2xl:flex-grow">
+      <section className="my-8 self-start w-full ">
+       <h3 className="text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl ml-0">
+  Auctions For Today
+</h3>
+
+<div className="flex flex-col items-start ml-0 flex-wrap gap-6">
+  <div
+    className="bg-[#161613] w-full p-3 rounded-md flex flex-col justify-center 
+    lg:flex-1 lg:h-auto lg:p-3 2xl:flex-none 2xl:basis-auto 2xl:flex-grow 2xl:px-3 2xl:py-2"
+  >
+    <div className="flex flex-col justify-center items-start gap-2">
+      <span className="rounded-full bg-[#fdba88] text-white w-fit p-3 mb-2">
+        <RiAuctionFill />
+      </span>
+
+      <h3
+        className="text-[#fdba88] text-xl font-semibold min-[480px]:text-xl 
+        md:text-2xl lg:text-3xl leading-tight"
+      >
+        Auctions For <br />
+        <span className="text-[#fff] inline-block mt-1">Today</span>
+      </h3>
+    </div>
+  </div>
+</div>
+
+          <div className="flex flex-col  gap-6 w-full lg:flex-1 2xl:flex-none 2xl:basis-64 2xl:flex-grow">
             {auctionsStartingToday.slice(0, 2).map((element) => {
               return (
                 <Link
@@ -50,12 +56,12 @@ const UpcomingAuctions = () => {
                       alt={element.title}
                       className="w-16 h-16 2xl:w-10 2xl:h-10"
                     />
-                    <p className="font-extralight text-[#111] text-[12px]">
+                    <p className="font-extralight text-[#111] text-2xl">
                       {element.title}
                     </p>
                   </div>
                   <div className="flex justify-between">
-                    <p className="text-stone-600 font-semibold">
+                    <p className="text-stone-600 text-xl font-semibold">
                       Starting Bid:
                     </p>{" "}
                     <p className="text-[#fdba88 font-semibold]">
@@ -63,8 +69,8 @@ const UpcomingAuctions = () => {
                     </p>{" "}
                   </div>
                   <div className="flex flex-col">
-                    <p className="text-stone-600 font-bold">Starting Time:</p>
-                    <p className="text-black text-[12px]">{element.startTime}</p>
+                    <p className="text-stone-600  text-xl font-semibold">Starting Time:</p>
+                    <p className="text-black text-[16px]">{element.startTime}</p>
                   </div>
                 </Link>
               );
@@ -92,9 +98,10 @@ const UpcomingAuctions = () => {
                     <p className="text-stone-600 font-semibold">
                       Starting Bid:
                     </p>{" "}
-                    <p className="text-[#fdba88 font-semibold]">
-                      Rs. {element.startingBid}
-                    </p>{" "}
+                    <p className="text-[#fdba88] font-medium text-[18px]">
+  Rs. {element.startingBid}
+</p>
+{" "}
                   </div>
                   <div className="flex flex-col">
                     <p className="text-stone-600 font-bold">Starting Time:</p>
@@ -137,7 +144,7 @@ const UpcomingAuctions = () => {
               );
             })}
           </div>
-        </div>
+       
       </section>
     </>
   );
