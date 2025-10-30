35ace4f24bc29625f054d708c3a3260083d75163
Mahima0529 <mahimaptel@gmail.com>
2025-10-30 19:16:32 +0530

    update ligin and sign-up page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,60 +32,60 @@ useEffect(()=>{
     <>
  <div className="flex min-h-screen">
   {/* Sidebar — only visible on large screens */}
-  <aside className="hidden lg:block w-[500px] bg-[#f6f4f0] fixed left-0 top-0 bottom-0">
-   
-  </aside>
+
 
   {/* Login Section */}
-  <section
+   <section
   className="
-    min-h-screen flex flex-col justify-center items-center
+    min-h-screen 
+    flex flex-col justify-center items-center
     bg-gradient-to-br from-[#FFD8A8] via-[#FFDAB9] to-[#F8D4DD]
     px-6 py-10 space-y-6
-    w-full
-    lg:ml-[500px] lg:w-[calc(100%-500px)]
+    h-[100vh]
+    w-[100vw] ml-0
+    md:w-[100vw] 
+    lg:w-[80vw] lg:ml-[20vw]
     overflow-y-auto
+    transition-all duration-300
   "
 >
 
+    <div className="bg-white mx-auto w-full h-auto px-2 flex flex-col gap-2 items-center py-4 justify-center rounded-md sm:w-[500px] sm:h-[370px]">
+        
+      <h1
+            className={`text-[#d6482b] text-2xl font-bold mb-2 min-[480px]:text-4xl md:text-5xl xl:text-4xl 2xl:text-4xl`}
+          >
+            Login
+          </h1>
 
-    <div
-      className="bg-white/95 backdrop-blur-md shadow-2xl rounded-2xl w-full 
-                 max-w-[900px]  h-[500px]  p-10 flex flex-col items-center gap-8 
-                 border border-[#f3b8c7]/30"
-    >
-      <h1 className="text-5xl font-extrabold text-[#D64838] drop-shadow-sm tracking-wide">
-        Login
-      </h1>
-
-      <form onSubmit={handleLogin} className="flex flex-col gap-6 w-full">
+      <form onSubmit={handleLogin} className="flex flex-col gap-5 w-full">
         <div className="flex flex-col gap-2">
-          <label className="text-3xl text-gray-800 font-semibold">Email</label>
+          <label className="text-[16px]  text-gray-800 ">Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="text-3xl py-3 bg-transparent border-b border-stone-500 
+            className="text-[16px] py-2 bg-transparent border-b border-stone-500 
                        focus:outline-none focus:border-[#D64838] transition-colors duration-200"
           />
         </div>
 
         <div className="flex flex-col gap-2">
-          <label className="text-3xl text-gray-800 font-semibold">Password</label>
+          <label className="text-[16px] text-gray-800 ">Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="text-[18px] py-3 bg-transparent border-b border-stone-500 
+            className="text-[16px] py-2 bg-transparent border-b border-stone-500 
                        focus:outline-none focus:border-[#D64838] transition-colors duration-200"
           />
         </div>
 
         <button
           type="submit"
-         className="mt-6 bg-gradient-to-r from-[#D64838] to-[#E65A50] text-white font-bold 
-             text-3xl py-4 px-8 rounded-xl shadow-lg hover:scale-105 hover:shadow-xl 
-             transition-all duration-300 w-auto mx-auto"
+         className="mt-6 bg-gradient-to-r from-[#D64838] to-[#E65A50] text-white font-semibold 
+             text-xl py-2 px-4 rounded-xl shadow-lg hover:scale-105 hover:shadow-xl 
+             transition-all duration-300 my-4 mx-auto"
         >
          {loading? "Logging In...":"Login"}
         </button>
