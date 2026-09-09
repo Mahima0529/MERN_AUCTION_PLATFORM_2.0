9388d7ad8027b44cd355c39320a9c33cdc3b3dd6
Mahima0529 <mahimaptel@gmail.com>
2026-09-09 09:49:19 +0530

    fix(auth): set secure: true and sameSite: 'none' for cross-domain cookie auth

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,9 @@ export const generateToken = (user, message, statuscode, res)=>{
     const token = user.generateJsonWebToken();
     res.status(statuscode).cookie("token", token, {
         expires: new Date(Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
     }).json({
         success:true,
         message,
