ae9ccb4ee7876fab7c2415afe3bdb3a6a9ed6ac6
Mahima Patel <mahimaptel@gmail.com>
2026-09-19 19:00:28 +0530

    Fix formatting in payment methods notification

--- automation/endedAuctionCron.js
+++ automation/endedAuctionCron.js
@@ -84,8 +84,8 @@ if (!alreadyWon) {
             \n- Account Number: ${auctioneer.paymentMethods.bankTransfer.bankAccountNumber}
              \n- Bank: ${auctioneer.paymentMethods.bankTransfer.bankName}\n\n2. **Googlepay**:
              \n- You can send payment via GooglePay: ${auctioneer.paymentMethods.googlepay.googlepay_upi_id}\n\n3. **Phonepe**:
-             \n- Send payment to: ${auctioneer.paymentMethods.phonepe.phonepe_upi_id}\n\n4. **Cash on Delivery (COD)**:\
-             n- If you prefer COD, you must pay 20% of the total amount upfront before delivery.\n- To pay the 20% upfront, use any of the above methods.
+             \n- Send payment to: ${auctioneer.paymentMethods.phonepe.phonepe_upi_id}\n\n4. **Cash on Delivery (COD)**:
+             \n- If you prefer COD, you must pay 20% of the total amount upfront before delivery.\n- To pay the 20% upfront, use any of the above methods.
              \n- The remaining 80% will be paid upon delivery.\n- If you want to see the condition of your auction item then send your email on this: ${auctioneer.email}
              \n\nPlease ensure your payment is completed by [Payment Due Date]. Once we confirm the payment, the item will be shipped to you.\n\nThank you for participating!
              \n\nBest regards,\nMAHIMA AUCTION TEAM`;
